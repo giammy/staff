3ddd326914afdd9dafe30fded445efd274fa51ad
Gianluca Moro <giangiammy@gmail.com>
2019-10-03 15:31:04 +0200

    added 3 fields to staff and complete staff import

---
+++ bin/import.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bin/console import:personale public/local/personale-2016.csv 2000 2016 
+bin/console import:personale public/local/personale-2017.csv 2017 2017
+bin/console import:personale public/local/personale-2018.csv 2018 2018
+bin/console import:personale public/local/personale-2019.csv 2019 2019
